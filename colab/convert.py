8214265c95919e2eedb79e525dcccf26fda29b17
johnpolsh <99343129+johnpolsh@users.noreply.github.com>
2023-11-13 12:59:45 -0300

    small fix

--- colab/convert.py
+++ colab/convert.py
@@ -3,12 +3,12 @@ from torch.utils.mobile_optimizer import optimize_for_mobile
 
 from torch.jit.mobile import _backport_for_mobile, _get_model_bytecode_version
 
-def convert_for_mobile(model, path, example=torch.rand(1, 3, 224, 224)):
+def convert_for_mobile(model, name, ext='.ptl', example=torch.rand(1, 3, 224, 224)):
     model.eval()
     traced_script_module = torch.jit.trace(model, example)
     optimized_traced_model = optimize_for_mobile(traced_script_module)
-    optimized_traced_model._save_for_lite_interpreter(path)
+    optimized_traced_model._save_for_lite_interpreter(f"tmp_{name}{ext}")
 
-    print(f"\nModel original version: {_get_model_bytecode_version(path)}")
-    _backport_for_mobile(f_input=path, f_output=f"android/{path}", to_version=5)
-    print(f"New model version: {_get_model_bytecode_version(path)}")
+    print(f"Model original version: ", _get_model_bytecode_version(f"tmp_{name}{ext}"))
+    _backport_for_mobile(f"tmp_{name}{ext}", f"{name}_v5{ext}", 5)
+    print(f"Model new version: ", _get_model_bytecode_version(f"{name}_v5{ext}"))
